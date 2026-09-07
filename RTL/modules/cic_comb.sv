53d82cd0f7586a843aca8a14fd3b609236883d9e
vovagroshev <93251026+vovagroshev@users.noreply.github.com>
2026-09-07 11:58:37 +0300

    Update cic_comb.sv

--- RTL/modules/cic_comb.sv
+++ RTL/modules/cic_comb.sv
@@ -1,65 +1,80 @@
-`timescale 1ns/1ps
+//`timescale 1ns/1ps
 
 module cic_comb #(
-    parameter int IN_WIDTH = 24,
+    parameter int IN_WIDTH  = 24,
     parameter int OUT_WIDTH = 24,
-    parameter int M = 1
+    parameter int M         = 1
 ) (
-    input logic clk,
-    input logic rst,
+    input  logic                            clk          ,
+    input  logic                            rst_n        ,
 
-    input logic signed [IN_WIDTH-1:0] din,
-    input logic din_valid,
+    input  logic signed [IN_WIDTH  - 1 : 0] i_din        ,
+    input  logic                            i_din_valid  ,
 
-    output logic signed [OUT_WIDTH-1:0] dout,
-    output logic dout_valid
+    output logic signed [OUT_WIDTH - 1 : 0] o_dout       ,
+    output logic                            o_dout_valid
 );
 
-    initial begin
-        if (OUT_WIDTH < IN_WIDTH) begin
-            $error("OUT_WIDTH must be >= IN_WIDTH");
-        end
+    logic signed [OUT_WIDTH - 1 : 0] din_ext           ;
 
-        if (M < 1) begin
-            $error("M must be >= 1");
-        end
-    end
+    logic signed [OUT_WIDTH - 1 : 0] delay_q_ff [0:M-1];
 
-    logic signed [OUT_WIDTH-1:0] din_ext;
-    logic signed [OUT_WIDTH-1:0] delay_q [0:M-1];
-    logic signed [OUT_WIDTH-1:0] diff_w;
+    logic signed [OUT_WIDTH - 1 : 0] dout_ff           ;
+    logic signed [OUT_WIDTH - 1 : 0] dout_next         ;
 
+    logic                            dout_valid_ff     ;
+    logic                            dout_valid_next   ;
+
+//--------------------------------------------------------------------------
+// input sign extension
     generate
         if (OUT_WIDTH > IN_WIDTH) begin : gen_sign_extend
-            assign din_ext = {{(OUT_WIDTH - IN_WIDTH){din[IN_WIDTH-1]}}, din};
+            assign din_ext = {{(OUT_WIDTH - IN_WIDTH){i_din[IN_WIDTH - 1]}}, i_din};
         end else begin : gen_no_extend
-            assign din_ext = din;
+            assign din_ext = i_din;
         end
     endgenerate
 
-    assign diff_w = din_ext - delay_q[M-1];
-
-    always_ff @(posedge clk or posedge rst) begin
-        if (rst) begin
-            dout <= '0;
-            dout_valid <= 1'b0;
-
+//--------------------------------------------------------------------------
+// comb delay
+    always_ff @(posedge clk, negedge rst_n) begin
+        if (~rst_n) begin
             for (int i = 0; i < M; i = i + 1) begin
-                delay_q[i] <= '0;
+                delay_q_ff[i] <= '0;
             end
-        end else begin
-            dout_valid <= din_valid;
+        end else if (i_din_valid) begin
+            delay_q_ff[0] <= din_ext;
 
-            if (din_valid) begin
-                dout <= diff_w;
-
-                delay_q[0] <= din_ext;
-
-                for (int i = 1; i < M; i = i + 1) begin
-                    delay_q[i] <= delay_q[i-1];
-                end
+            for (int i = 1; i < M; i = i + 1) begin
+                delay_q_ff[i] <= delay_q_ff[i - 1];
             end
         end
     end
 
-endmodule
+//--------------------------------------------------------------------------
+// dout management
+    assign dout_next       = din_ext - delay_q_ff[M - 1];
+    assign dout_valid_next = i_din_valid;
+
+    always_ff @(posedge clk, negedge rst_n) begin
+        if (~rst_n) begin
+            dout_ff       <= '0;
+            dout_valid_ff <= '0;
+        end else begin
+            dout_valid_ff <= dout_valid_next;
+
+            if (i_din_valid) begin
+                dout_ff <= dout_next;
+            end
+        end
+    end
+
+//--------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------
+// output ports
+    assign o_dout       = dout_ff;
+    assign o_dout_valid = dout_valid_ff;
+//--------------------------------------------------------------------------
+
+endmodule : cic_comb

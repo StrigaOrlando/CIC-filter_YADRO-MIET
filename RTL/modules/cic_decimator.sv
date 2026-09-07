5ac53b891295c4fc97dbafb53649b6e6343535ef
vovagroshev <93251026+vovagroshev@users.noreply.github.com>
2026-09-07 11:59:41 +0300

    Update cic_decimator.sv

--- RTL/modules/cic_decimator.sv
+++ RTL/modules/cic_decimator.sv
@@ -1,47 +1,78 @@
-`timescale 1ns/1ps
+//`timescale 1ns/1ps
 
 module cic_dec #(
     parameter int DATA_WIDTH = 24,
-    parameter int R = 4
+    parameter int R          = 4
 ) (
-    input logic clk,
-    input logic rst,
+    input  logic                             clk          ,
+    input  logic                             rst_n        ,
 
-    input logic signed [DATA_WIDTH-1:0] din,
-    input logic din_valid,
+    input  logic signed [DATA_WIDTH - 1 : 0] i_din        ,
+    input  logic                             i_din_valid  ,
 
-    output logic signed [DATA_WIDTH-1:0] dout,
-    output logic dout_valid
+    output logic signed [DATA_WIDTH - 1 : 0] o_dout       ,
+    output logic                             o_dout_valid
 );
-
-    initial begin
-        if (R < 1) begin
-            $error("R must be >= 1");
-        end
-    end
 
     localparam int CNT_WIDTH = (R <= 1) ? 1 : $clog2(R);
 
-    logic [CNT_WIDTH-1:0] cnt_q;
+    logic        [ CNT_WIDTH - 1 : 0] cnt_q_ff         ;
+    logic        [ CNT_WIDTH - 1 : 0] cnt_q_next       ;
 
-    always_ff @(posedge clk or posedge rst) begin
-        if (rst) begin
-            cnt_q <= '0;
-            dout <= '0;
-            dout_valid <= 1'b0;
+    logic                             dout_valid_ff    ;
+    logic                             dout_valid_next  ;
+
+    logic signed [DATA_WIDTH - 1 : 0] dout_ff          ;
+
+    logic                             cnt_is_max       ;
+
+//------------------------------------------------------------
+// counter
+    assign cnt_is_max = (cnt_q_ff == (R - 1));
+
+    // variant 1
+//    always_comb begin
+//        cnt_q_next = cnt_q_ff + 1'b1;
+//        if (i_din_valid & cnt_is_max) begin
+//            cnt_q_next = '0;
+//        end
+//    end
+
+    // variant 2
+    assign cnt_q_next = (i_din_valid & cnt_is_max) ? '0 : cnt_q_ff + 1'b1;
+
+    always_ff @(posedge clk, negedge rst_n) begin
+        if (~rst_n) begin
+            cnt_q_ff <= '0;
+        end else if (i_din_valid) begin
+            cnt_q_ff <= cnt_q_next;
+//          cnt_q_ff <= (i_din_valid & cnt_is_max) ? '0 : cnt_q_ff + 1'b1; // not bad too
+        end
+    end
+//------------------------------------------------------------
+
+//------------------------------------------------------------
+// dout managment
+    assign dout_valid_next = i_din_valid & cnt_is_max;
+
+    always_ff @(posedge clk, negedge rst_n) begin
+        if (~rst_n) begin
+            dout_valid_ff <= '0;
+            dout_ff       <= '0;
         end else begin
-            dout_valid <= 1'b0;
+            dout_valid_ff <= dout_valid_next;
 
-            if (din_valid) begin
-                if (cnt_q == R - 1) begin
-                    dout <= din;
-                    dout_valid <= 1'b1;
-                    cnt_q <= '0;
-                end else begin
-                    cnt_q <= cnt_q + 1'b1;
-                end
+            if (i_din_valid) begin
+                dout_ff <= i_din;
             end
         end
     end
+//------------------------------------------------------------
 
-endmodule
+//------------------------------------------------------------
+// output ports
+    assign o_dout       = dout_ff;
+    assign o_dout_valid = dout_valid_ff;
+//------------------------------------------------------------
+
+endmodule : cic_dec
